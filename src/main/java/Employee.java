19cf5938b9197ea820191a1763e21751dff6a713
Edvold <56292479+Edvold@users.noreply.github.com>
2022-04-19 23:51:18 +0200

    Added method for getting availability of employee

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -6,18 +7,37 @@ public class Employee {
 
     private String name;
     private HashMap<Project, Double> projectHours = new HashMap<>();
+    private ArrayList<Activity> activities = new ArrayList<>();
 
     public Employee(String name) {
         this.name = name;
     }
 
     public boolean isAvailable(LocalDateTime startDate, LocalDateTime endDate) {
-        // NOT IMPLEMENTED
-        return false;
+        Set<Project> projects = projectHours.keySet();
+        for (Project project : projects) {
+            if(!project.getProjectLeader().equals(this)) continue;
+
+            if(isBetweenDates(project.getStartDate(), project.getLastEndDate(), startDate, endDate)) return false;
+        }
+
+        for (Activity activity : activities) {
+            if (isBetweenDates(activity.getStartDate(), activity.getEndDate(), startDate, endDate)) return false;
+        }
+
+        return true;
+    }
+
+    private boolean isBetweenDates(LocalDateTime eventStartDate, LocalDateTime eventEndDate, LocalDateTime startDate, LocalDateTime endDate) {
+        return (eventStartDate.isAfter(startDate) && eventStartDate.isBefore(endDate)) || (eventEndDate.isBefore(endDate) && eventStartDate.isAfter(startDate));
+    }
+
+    public void addActivity(Activity activity) {
+        activities.add(activity);
     }
 
     public boolean isWorkingOnActivity(Activity activity) {
-        return (getProjectByActivity(activity) != null);
+        return activities.contains(activity);
     }
 
     public Project getProjectByActivity(Activity activity) {
